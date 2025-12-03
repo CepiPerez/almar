52c9c01736c088116f4999fb5381d15cf7d55288
Matias Perez <cepiperez@gmail.com>
2025-12-03 19:27:08 -0300

    Fix idioma

--- assets/js/idioma.js
+++ assets/js/idioma.js
@@ -1,23 +1,9 @@
-const langBtn = document.getElementById("lang-btn");
-const langMenu = document.getElementById("lang-menu");
 const currentLangSpan = document.getElementById("current-lang");
 
 function changeLang(lang) {
     localStorage.setItem("lang", lang);
     window.location.href = "../" + lang + "/index.html";
 }
-
-// Mostrar / ocultar menú
-langBtn.addEventListener("click", () => {
-    langMenu.classList.toggle("hidden");
-});
-
-// Cerrar si hace click afuera
-document.addEventListener("click", (e) => {
-    if (!document.getElementById("lang-dropdown").contains(e.target)) {
-        langMenu.classList.add("hidden");
-    }
-});
 
 // Setear idioma actual al cargar
 (function () {
